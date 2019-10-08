c5e73c52047d04c602d82d9652f6d007eb037790
Bruce Lee <bruce.l640@stu.nebo.edu>
2019-10-08 10:30:28 -0600

    Started prototype 2 added player control and boundaries

---
+++ CreateWithCode/Prototype 2/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        //boundaries for player so they don't fly off of screen
+         if (transform.position.x < -10.0f)
+        {
+            transform.position = new Vector3(-10.0f, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > 10.0f)
+        {
+            transform.position = new Vector3(10.0f, transform.position.y, transform.position.z);
+        }
+        //player moves left and right using arrow keys
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+    }
+}
