3c36e0ecf7394fe3f8804fc46e1eb807cf24799b
Bruce Lee <bruce.l640@stu.nebo.edu>
2019-10-10 10:28:21 -0600

    added gfuel shooter

--- CreateWithCode/Prototype 2/Assets/Scripts/PlayerControler.cs
+++ CreateWithCode/Prototype 2/Assets/Scripts/PlayerControler.cs
@@ -7,21 +7,28 @@ public class PlayerControler : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public GameObject projectilePrefab;
     // Update is called once per frame
     void Update()
     {
         //boundaries for player so they don't fly off of screen
-         if (transform.position.x < -10.0f)
+         if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10.0f, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-        if (transform.position.x > 10.0f)
+        if (transform.position.x > xRange)
         {
-            transform.position = new Vector3(10.0f, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         //player moves left and right using arrow keys
         horizontalInput = Input.GetAxis("Horizontal");
 
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+   
+        }
     }
 }
