82629434a644cd3d5cc1e6ff876e7022dc7d82f5
Bruce Lee <bruce.l640@stu.nebo.edu>
2019-10-16 10:29:55 -0600

    Did the collision stuff

--- CreateWithCode/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public       
-    // Start is called before the first frame update
+    private float spawnRangeX = 15;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, 1.5f);
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
+ 
+    }
+    void SpawnRandomAnimal()
+    {
+       
         {
             int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 25),
-                animalPrefabs[animalIndex].transform.rotation);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
