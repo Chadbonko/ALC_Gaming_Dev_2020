d9dbd1a91342593533e462f4da21373d3e57b7fd
Bruce Lee <bruce.l640@stu.nebo.edu>
2019-12-10 10:30:00 -0700

    made progress into unit 4.3

--- CreateWithCode/Prototype 4/Assets/Course Library/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 4/Assets/Course Library/Scripts/PlayerController.cs
@@ -6,9 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     public float speed;
+    private GameObject focalPoint;
+    public bool hasPowerup;
     // Start is called before the first frame update
     void Start()
     {
+        focalPoint = GameObject.Find("Focal Point");
         playerRb = GetComponent<Rigidbody>();
         {
 
@@ -19,5 +22,25 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Player collided with " + collision.gameObject + " with powerup set to " + hasPowerup);
+        }
     }
 }
